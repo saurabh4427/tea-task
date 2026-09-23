36ab9cf5f25d8aa88c498af46973236683c704ea
saraubh anand <tresna0928@gmail.com>
2026-09-23 22:41:37 +0530

    commit

---
+++ task1.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("Welcome to the AWS Club!\n");
+    return 0;
+}
---
+++ web/script.js
@@ -0,0 +1,9 @@
+const button = document.getElementById("welcome-btn");
+const message = document.getElementById("message");
+
+button.addEventListener
+{("click", function () {
+  message.textContent = "Hello from Team CHANGE_ME! Welcome to the AWS Club!";
+});
+}
+  
